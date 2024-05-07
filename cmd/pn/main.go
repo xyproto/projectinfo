b6c1f490fca9a127176b27e2fd9c0c5d3c16c9eb
Alexander F. Rødseth <rodseth@gmail.com>
2024-05-07 16:07:14 +0200

    Add an example utility

---
+++ cmd/pn/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/xyproto/projectname"
+)
+
+func main() {
+	// Check for command line arguments
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: main [directory]")
+		os.Exit(1)
+	}
+
+	// The first argument should be the directory to scan
+	dir := os.Args[1]
+
+	// Use the ReadProjectName function from the projectname package
+	name, err := projectname.ReadProjectName(dir)
+	if err != nil {
+		fmt.Printf("Failed to read project name: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Output the found project name
+	fmt.Printf("Project name: %s\n", name)
+}
