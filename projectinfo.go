fa3cee0a9003f29632e5042db828e705c8a5ff54
Alexander F. Rødseth <rodseth@gmail.com>
2024-05-27 16:12:30 +0200

    Remove unused functionality + fix the AllFiles function

--- projectinfo.go
+++ projectinfo.go
@@ -1,8 +1,6 @@
 package projectinfo
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"path/filepath"
 	"strings"
@@ -17,16 +15,6 @@ type ProjectInfo struct {
 	Type            string     `json:"type"`
 	Contributors    string     `json:"contributors"`
 	APIServer       bool       `json:"apiServer"`
-}
-
-var maxTokensPerChunk = 250000 // This constant defines the maximum token count per chunk of JSON output to avoid excessive data in one batch.
-
-func SetMaxTokensPerChunk(n int) {
-	maxTokensPerChunk = n
-}
-
-func MaxTokensPerChunk() int {
-	return maxTokensPerChunk
 }
 
 func New(dir string, outputWarnings bool) (ProjectInfo, error) {
@@ -78,46 +66,7 @@ func New(dir string, outputWarnings bool) (ProjectInfo, error) {
 }
 
 func (project *ProjectInfo) AllFiles() []FileInfo {
-	return append(project.SourceFiles, project.ConfAndDocFiles...)
-}
-
-// Chunk breaks down project information into manageable JSON chunks to adhere to token limitations
-func (project *ProjectInfo) Chunk(includeSourceFiles, includeConfAndDocFiles bool) ([]string, error) {
-	var (
-		chunks            []string
-		currentChunk      []FileInfo
-		currentTokenCount = 0
-		files             = []FileInfo{}
-	)
-	if includeSourceFiles {
-		files = append(files, project.SourceFiles...)
-	}
-	if includeConfAndDocFiles {
-		files = append(files, project.ConfAndDocFiles...)
-	}
-	for _, file := range files {
-		file.TokenCount = CountTokens(file.Contents) // Compute token count for each file, assuming this function is defined in utils.go.
-		if currentTokenCount+file.TokenCount > maxTokensPerChunk {
-			// Finalize the current chunk and reset counters if the maximum token count is exceeded.
-			chunkData, err := json.Marshal(currentChunk)
-			if err != nil {
-				return nil, fmt.Errorf("error marshaling chunk: %v", err)
-			}
-			chunks = append(chunks, string(chunkData))
-			currentChunk = []FileInfo{} // Reset the current chunk
-			currentTokenCount = 0
-		}
-		// Add the file to the current chunk
-		currentChunk = append(currentChunk, file)
-		currentTokenCount += file.TokenCount
-	}
-	// Add the last chunk if it contains any files
-	if len(currentChunk) > 0 {
-		chunkData, err := json.Marshal(currentChunk)
-		if err != nil {
-			return nil, fmt.Errorf("error marshaling final chunk: %v", err)
-		}
-		chunks = append(chunks, string(chunkData))
-	}
-	return chunks, nil
+	var files []FileInfo
+	files = append(files, project.SourceFiles...)
+	return append(files, project.ConfAndDocFiles...)
 }
